13a897d9e300188be0a6a315f4683ff4eb94bd8b
WatIsDeze <watisdeze2@gmail.com>
2019-10-16 19:44:34 +0200

    nothing added to commit but untracked files present so yeah

---
+++ game/CMakeFiles/SchizoMania_Game_Client.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/commands.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/commands.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/basemapmodel.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/basemapmodel.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/basemonster.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/basemonster.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/door.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/door.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/dynamiclight.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/dynamiclight.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/player.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/player.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities/playerstart.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/entities/playerstart.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/game.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/game.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/render.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/render.cpp.o"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/server.cpp" "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/game/CMakeFiles/SchizoMania_Game_Client.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GL_SILENCE_DEPRECATION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/ext/json-develop/single_include"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/.."
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/../engine"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/../game"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/../shared"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/../shared/entities"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/game/entities"
+  "/usr/include/SDL2"
+  "/usr/local/include/freetype2"
+  "/home/watisdeze/Development/Projects/SchizoMania/Clones/ScMa/src/enet/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/shared/CMakeFiles/SchizoMania_Shared_Client.dir/DependInfo.cmake"
+  "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/shared/entities/CMakeFiles/SchizoMania_Shared_Entities.dir/DependInfo.cmake"
+  "/home/watisdeze/Development/Projects/SchizoMania/Builds/Debug/enet/CMakeFiles/enet.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
