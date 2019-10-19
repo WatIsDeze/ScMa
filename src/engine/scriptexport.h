39d4db168fc092e0a1485e1fe128fba25547cd83
Micha <michavankesteren@gmail.com>
2019-10-20 01:48:45 +0200

    scriptexport cleanup

--- src/engine/scriptexport.h
+++ src/engine/scriptexport.h
@@ -9,11 +9,3 @@
 
 #define SCRIPTEXPORT __attribute__((annotate("scriptexport"))) VISIBLE_SYMBOL
 #define SCRIPTEXPORT_AS(NAME) __attribute__((annotate("scriptexport" #NAME))) VISIBLE_SYMBOL
-
-class ScriptBindStaticInitializer
-{
-public:
-	static ScriptBindStaticInitializer& Get();
-    void Register(std::function<void()> initializer);
-    void Execute();
-};
